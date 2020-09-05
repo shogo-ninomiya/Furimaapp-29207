3e4578a3ad91ed2908da0d3081190f05627ba696
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-05 11:35:48 +0900

    バリデーションの設定　
    エラーメッセージは確認後、再コミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+VALID_PASSWORD_REGEX =/\A[a-z0-9]+\z/i
+
 validates :nickname, presence: true
+validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
 validates :first_name, presence: true
 validates :last_name, presence: true
-validates :first_name_kana, presence: true
-validates :last_name_kana, presence: true
+validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 validates :birthday, presence: true
 
 
