e8cc694e4a911c058973feede4d806e95f774ab8
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-06 21:41:22 +0900

    単体テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
 VALID_PASSWORD_REGEX =/\A[a-z0-9]+\z/i
 
 validates :nickname, presence: true
-validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
+validates :password ,presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
 validates :first_name, presence: true, format: { with: /\A[一-龥]+\z/, message: 'Full-width characters'}
 validates :last_name, presence: true, format: { with: /\A[一-龥]+\z/, message: 'Full-width characters'}
-validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width characters'}
-validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width characters'}
+validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}
+validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}
 validates :birthday, presence: true
 
 
