f08ff2b0888c5afb9200ea4ce2e52aa8fc7be2b7
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-09 18:43:09 +0900

    テストコード記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,11 @@ class Item < ApplicationRecord
   has_one_attached :image
 
 
+  VALID_PRICE_REGEX = /\A[-]?[0-9]+(\.[0-9]+)?\z/
+
   validates :name, presence: true
   validates :comment, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, :numericality => {  :less_than_or_equal_to => 9999999,:greater_than_or_equal_to => 300 }, format: { with: VALID_PRICE_REGEX }
   validates :category_id, presence: true
   validates :status_id, presence: true
   validates :delivery_id, presence: true
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name        { 'サンプル' }
+    comment     { 'sample' }
+    price       {2000}
+    category_id {2}
+    status_id   {2}
+    delivery_id {2}
+    area_id     {2}
+    day_id      {2}
+    user_id     {2}
+    association :user
+
+      # rspecでuserとのアソシエーションを組みましょう
+      # 検索がうまくヒットしない場合はfactorybot アソシエーションで調べましょう！
   end
 end
