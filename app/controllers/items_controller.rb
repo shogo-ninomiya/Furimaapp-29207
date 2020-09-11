bd0d42a1a1f4d59ee5242bd715e092ca830c9be1
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-11 16:44:40 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
     if item.update(item_params)
       redirect_to item_path(item.id)
     else
-      render :new
+      render :item_path(item.id)
     end
   end
 
