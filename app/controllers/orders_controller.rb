1b0cc2b08f57b64cd7877ce53c2d6ce5b53465f1
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-14 12:20:36 +0900

    データベース　db:migrate

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,23 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :move_to_index, except: [:index]
+  # before_actionにて二番の質問の回答
 
 
   def index 
     @item = Item.find(params[:item_id])
   end
 
-  
+  def create
+    #form_objをつかって保存する動きをとる
+  end
+
 
   private
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to action: :root_path
     end
   end
 end
