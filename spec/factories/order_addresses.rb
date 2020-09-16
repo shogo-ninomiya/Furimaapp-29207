e83d7ba97c3499d42256836e7f5c1cbb63b6c85e
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-16 20:51:52 +0900

    テストコードの実装

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_address do
+    post_code { "111-1111" }
+    prefecture_id { 5 }
+    city { '立川市' }
+    block { '1-1' }
+    building { '東京ハイツ' }
+    tellphone { '09090909090' }
+    token { 'qawsdefrgthyjuiko1234567' }
+    association :user
+    association :item
+  end
+end
