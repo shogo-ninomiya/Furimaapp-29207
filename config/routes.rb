ba2578c01ac0afe40954067b8dcb081ba87f02d5
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-21 17:19:13 +0900

    ネストルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
   resources :items do
+    resources :comments, only: [:create]
     resources :orders, only: [:index, :create,]
   end
+  resources :comments, only: [:create]
 end
