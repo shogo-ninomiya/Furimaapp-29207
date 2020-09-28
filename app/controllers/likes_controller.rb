eef20a27263d8682fff4c282bab3fcce6dd3cdff
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-28 21:50:56 +0900

    非同期通信のいいね機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,17 +5,18 @@ class LikesController < ApplicationController
   def create
     user = current_user
     item = Item.find(params[:item_id])
-    like = Like.create(user_id: user.id, item_id: item.id)
+    like = Like.create(user_id: user.id, item_id: @item.id)
   end
+
   def destroy
     user = current_user
-    post = Post.find(params[:post_id])
-    like = Like.find_by(user_id: user.id, item_id: item.id)
+    item = Item.find(params[:item_id])
+    like = Like.find_by(user_id: user.id, item_id: @item.id)
     like.delete
   end
-  
+
   private
   def set_like
-      @post = Post.find(params[:post_id])
+    @item = Item.find(params[:item_id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,9 @@ class Item < ApplicationRecord
   has_many :likes
   has_many :users, through: :likes
 
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 
   def self.search(search)
     if search != ''
