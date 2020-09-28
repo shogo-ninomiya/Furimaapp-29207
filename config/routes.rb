448d10aa2bd22fb716516f8148fbadbc1779ff79
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-28 14:30:39 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :items do
     resources :comments, only: [:create]
     resources :orders, only: [:index, :create,]
+    resources :likes, only: [:create, :destroy]
     collection do
       get 'search'
     end
