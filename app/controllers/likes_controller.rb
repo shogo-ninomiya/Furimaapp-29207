993aac190df79df46ebf5c341ebb94af74fd3974
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-28 14:48:27 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -13,5 +13,9 @@ class LikesController < ApplicationController
     like = Like.find_by(user_id: user.id, item_id: item.id)
     like.delete
   end
-
+  
+  private
+  def set_like
+      @post = Post.find(params[:post_id])
+  end
 end
