da8de09e8a49e15a6cc8ebdbf23012a78cd80f72
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-09-29 22:39:40 +0900

    お気に入り商品のマイページ反映

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @items = user.items
+
+    @user = User.find(params[:id])
+    
   end
 end
